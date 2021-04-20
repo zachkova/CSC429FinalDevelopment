ca00443fa1552a40f7065a097856c9c890293ebb
zachkova <zachkova@gmail.com>
2021-04-20 17:44:51 -0400

    Deleting Main

---
+++ src/GAAAAANG/Hunter.java
@@ -0,0 +1,8 @@
+package GAAAAANG;
+
+class Hunter{
+
+    public static void main (String [] args){
+        System.out.println("Everything is purple");
+    }
+}
---
+++ src/GAAAAANG/Main.java
@@ -0,0 +1,2 @@
+package GAAAAANG;public class Main {
+}
